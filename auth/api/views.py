82a1b4e6055eabf45b25e31266c56a9d25eeaac8
GesaLoof <gesaloof@gmail.com>
2026-06-16 12:06:38 +0200

    created gitignore for db and secret keys

---
+++ auth/api/views.py
@@ -0,0 +1,63 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer, CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+class RegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+
+        data = {}
+        if serializer.is_valid():
+            saved_account = serializer.save()
+            data = {
+                'username': saved_account.username,
+                'email': saved_account.email,
+                'user_id': saved_account.pk
+            }
+            return Response(data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class CookieTokenObtainPairView(TokenObtainPairView):
+
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        refresh = serializer.validated_data['refresh']
+        access = serializer.validated_data['access']
+        response = Response()
+        response.set_cookie(key='refresh_token', value=refresh, httponly=True, secure=True, samesite='Lax')
+        response.set_cookie(key='access_token', value=access, httponly=True, secure=True, samesite='Lax')
+
+        response.data = {
+            'message': 'Login successful'
+        }
+        return response
+    
+
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get('refresh_token')
+        if not refresh_token:
+            return Response({'error': 'Refresh token not found in cookies'}, status=status.HTTP_400_BAD_REQUEST)
+
+        data = {'refresh': refresh_token}
+        serializer = self.get_serializer(data=data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+
+        access_token = serializer.validated_data.get('access')
+        response = Response({'message': 'Token refreshed successfully'})
+        response.set_cookie(key='access_token', value=access_token, httponly=True, secure=True, samesite='Lax')
+        return response
--- quizly/settings.py
+++ quizly/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'auth.api',
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
+}
