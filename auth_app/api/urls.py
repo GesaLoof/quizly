9e13cf6d70de59e468bf8198135cac8b907e46e6
GesaLoof <gesaloof@gmail.com>
2026-06-19 09:31:30 +0200

    changed registration url

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -8,7 +8,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("registration/", RegistrationView.as_view(), name="registration"),
+    path("register/", RegistrationView.as_view(), name="registration"),
     path("login/", CookieTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("logout/", LogoutView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", CookieTokenRefreshView.as_view(), name="token_refresh"),
