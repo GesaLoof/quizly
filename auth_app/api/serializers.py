2b5e9d948f1d173006ee8f80742846145c730960
GesaLoof <gesaloof@gmail.com>
2026-06-22 17:53:59 +0200

    changed 'repeated_password' to 'confirmed_password' in registration serializer

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -12,7 +12,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ["username", "email", "password", "confirmed_password"]
         extra_kwargs = {"password": {"write_only": True}, "email": {"required": True}}
 
-    def validate_repeated_password(self, value):
+    def validate_confirmed_password(self, value):
         password = self.initial_data.get("password")
         if password and value and password != value:
             raise serializers.ValidationError("Passwords do not match")
